7bab91db8d030804f6281611e8c5bab1e65ba324
Alekseyua <alekseyuadnepr@gmail.com>
2023-06-22 13:57:26 +0300

    change config

--- #src/js/app.js
+++ #src/js/app.js
@@ -1,13 +1,14 @@
 
-	/*Это должно быть помещено в документе голове , лучше всего перед первой <script>встречаемостью*/
+	/*
+	Это должно быть помещено в документе голове , лучше всего перед первой <script>встречаемостью
+		
+	*/
 
 
 
 window.onload = function() {
-	@@include('files/_swiper-bundle.js',{});
-	@@include('files/_functions.js',{});
-	@@include('files/_script.js',{});
-	@@include('files/_sliders.js',{});
+
+	@@include('./files/_script.js',{});
 
 }
 
